03c00ec7dfa4602cb32eed75d2b5bc13dcb0fc20
Jignesh Patel <p.jignesh23@gmail.com>
2022-02-21 20:47:53 +0530

    feat: update counter actions with firebase and counter repository

--- lib/counter/bloc/counter_bloc.dart
+++ lib/counter/bloc/counter_bloc.dart
@@ -5,9 +5,13 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 
-enum CounterEvent { increment, decrement }
+import 'package:bloc/bloc.dart';
+import 'package:counter_repository/counter_repository.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_events.dart';
 
 mixin Counter1Bloc on Bloc<CounterEvent, int> {}
 
@@ -17,18 +21,46 @@ mixin Counter3Bloc on Bloc<CounterEvent, int> {}
 
 class CounterBloc extends Bloc<CounterEvent, int>
     with Counter1Bloc, Counter2Bloc, Counter3Bloc {
-  CounterBloc() : super(0) {
-    on<CounterEvent>(_onCounterEvent);
+  CounterBloc({
+    required this.key,
+    required this.counterRepository,
+  }) : super(0) {
+    _streamSub = counterRepository
+        .onValue(key)
+        .listen((value) => add(OnValueCounterEvent(value)));
+    on<IncrementCounterEvent>(_onIncrementCounterEvent);
+    on<DecrementCounterEvent>(_onDecrementCounterEvent);
+    on<OnValueCounterEvent>(_onOnValueCounterEvent);
   }
 
-  void _onCounterEvent(CounterEvent event, Emitter<int> emit) {
-    switch (event) {
-      case CounterEvent.increment:
-        emit(state + 1);
-        break;
-      case CounterEvent.decrement:
-        emit(state - 1);
-        break;
-    }
+  final CounterRepository counterRepository;
+  final String key;
+  late StreamSubscription _streamSub;
+
+  void _onIncrementCounterEvent(
+    IncrementCounterEvent event,
+    Emitter<int> emit,
+  ) {
+    counterRepository.set(key, state + 1);
+  }
+
+  void _onDecrementCounterEvent(
+    DecrementCounterEvent event,
+    Emitter<int> emit,
+  ) {
+    counterRepository.set(key, state - 1);
+  }
+
+  void _onOnValueCounterEvent(
+    OnValueCounterEvent event,
+    Emitter<int> emit,
+  ) {
+    emit(event.value);
+  }
+
+  @override
+  Future<void> close() {
+    _streamSub.cancel();
+    return super.close();
   }
 }
